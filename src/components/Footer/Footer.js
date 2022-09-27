e5c376e493bcfd915ab9fa91a20ec76d27de8318
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2022-09-27 17:01:35 +0300

    update footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,12 @@
 import footerBg from './footer-bg.png';
-import gitcoingCommunity from './gitcoin-community.svg';
 import './Footer.css';
 
 function Footer () {
-    return (
-        <footer>
-            <img className='footer-gitcoin' src={gitcoingCommunity} alt="" width="163" height="34" />
-            <img className='footer-bg' src={footerBg} alt="" width="1440" height="252"/>
-        </footer>
-    );
+  return (
+    <footer>
+      <img className='footer-bg' src={footerBg} alt="" width="1440" height="252"/>
+    </footer>
+  );
 }
 
 export default Footer ;
