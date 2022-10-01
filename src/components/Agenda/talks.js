d1b2f18850ef3c911886a4d2370df366625c646a
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2022-10-01 23:18:32 +0300

    update agenda

--- src/components/Agenda/talks.js
+++ src/components/Agenda/talks.js
@@ -2,7 +2,7 @@ export const talksMain = [
   {
     time: '10:00 AM',
     title: 'Welcome to Schelling Point',
-    name: 'Simona Pop',
+    name: `Simona Pop ◖ SP Content Curator`,
     calendar: 'https://calendar.google.com/calendar/render?action=TEMPLATE&dates=20221010T150000Z%2F20221010T151000Z&details=&location=gran%20carpa%20am%C3%A9ricas%20corferias&text=Welcome%20to%20Schelling%20Point%20%28Simona%20Pop%29',
   },
   {
@@ -14,31 +14,31 @@ export const talksMain = [
   {
     time: '10:20 AM',
     title: 'Attention is all you need',
-    name: 'Kevin Owocki',
+    name: 'Kevin Owocki ◖ Gitcoin',
     calendar: ''
   },
   {
     time: '10:40 AM',
     title: 'Designing DevDAOs',
-    name: 'Abbey Titcomb',
+    name: 'Abbey Titcomb ◖ Radicle',
     calendar: ''
   },
   {
     time: '11:00 AM',
     title: 'Creating welcoming, diverse, and sustainable developer communities',
-    name: 'Kasia Sitkiewicz, Camila Ramos, Nader Dabit',
+    name: 'Kasia Sitkiewicz ◖ Gitcoin, Camila Ramos ◖ Fuel Labs, Nader Dabit ◖ DevDAO, Xochitl Cazador ◖ Celo',
     calendar: ''
   },
   {
     time: '11:30 AM',
     title: 'Why no one is using Blockchain?!',
-    name: 'Fabian Vogelsteller',
+    name: 'Fabian Vogelsteller ◖ LUKSO',
     calendar: ''
   },
   {
     time: '11:55 AM',
     title: 'Plurality: Technology for Collaborative Diversity and Democracy',
-    name: 'Glen Weyl',
+    name: 'Glen Weyl ◖ RadicalxChange',
     calendar: ''
   },
   {
@@ -50,67 +50,67 @@ export const talksMain = [
   {
     time: '12:55 PM',
     title: 'Using social capital to coordinate digital economies',
-    name: 'Danny Zuckerman',
+    name: 'Danny Zuckerman ◖ ',
     calendar: ''
   },
   {
     time: '1:15 PM',
     title: 'Decentralized social media - a public good?',
-    name: 'Manasi Vora',
+    name: 'Manasi Vora ◖ Skynet',
     calendar: ''
   },
   {
     time: '1:35 PM',
     title: 'Cryoto Artistry and Collective Transformation',
-    name: '"Shivani Mitra, Julian Brangold, Stefany Cuervo, Bertie Lumsden, Rico Serna',
+    name: 'Shivani Mitra ◖ MOCA, Julian Brangold ◖ CruptoArg, Stefany Cuervo ◖ Colombian Artist, Bertie Lumsden ◖ ArtDAO, Rico Serna ◖ Chimbites',
     calendar: ''
   },
   {
     time: '2:05 PM',
     title: 'Communication is a Public Good',
-    name: 'Harsh Rajat',
+    name: 'Harsh Rajat ◖ EPNS',
     calendar: ''
   },
   {
     time: '2:25 PM',
     title: 'Open source as a public good',
-    name: 'Jahed Momand, Linda Xie, Awa Lefteris Karapetsas, Scott Moore, Awa Sun Yin',
+    name: 'Jahed Momand ◖ Cerulean Ventures, Linda Xie ◖ Scalar Capital, Awa Lefteris Karapetsas ◖ Rotki, Scott Moore ◖ Gitcoin, Awa Sun Yin ◖ Anoma',
     calendar: ''
   },
   {
     time: '2:55 PM',
     title: 'Data Dating',
-    name: 'Evin McMullen',
+    name: 'Evin McMullen ◖ Disco',
     calendar: ''
   },
   {
     time: '3:20 PM',
     title: 'You Just DAO-ed it, now what?',
-    name: 'Denison Bertram, James Waugh, Kris Decoodt, Simona Pop',
+    name: 'Denison Bertram ◖ Tally, James Waugh ◖ fireeyes, Kris Decoodt ◖ Gitcoin, Simona Pop ◖ Schelling Point',
     calendar: ''
   },
   {
     time: '3:55 PM',
     title: `Introducing Re:Generate`,
-    name: 'Rahul Kothari',
+    name: 'Rahul Kothari ◖ Reddit',
     calendar: ''
   },
   {
     time: '4:15 PM',
     title: 'Onboarding Latam users and builders in the future of Web3',
-    name: 'Pablo Sabbatella',
+    name: 'Pablo Sabbatella ◖ ',
     calendar: ''
   },
   {
     time: '4:35 PM',
     title: `Hot DAO Coordination Autumn - Let's figure out how your DAO actually coordinates`,
-    name: 'Aaron Soskin',
+    name: 'Aaron Soskin ◖ govrn.io',
     calendar: ''
   },
   {
     time: '4:55 PM',
     title: `Regenerative Employment is the Future of Work`,
-    name: 'John Paller',
+    name: 'John Paller ◖ Opolis',
     calendar: ''
   },
 ];
@@ -119,37 +119,37 @@ export const talksPlurality = [
   {
     time: '10:00 AM',
     title: 'Web3 & Plurality Brunch hosted by RadicalxChange & Fundación Karisma',
-    name: 'Paula Berman, Andrés Velásquez, Daniel Knobelsdorf',
+    name: 'Paula Berman ◖ RadicalxChange, Andrés Velásquez ◖ Fundación Karisma, Daniel Knobelsdorf ◖ RadicalxChange',
     calendar: ''
   },
   {
     time: '11:55 AM',
     title: '[ MAIN STAGE ] Plurality: Technology for Collaborative Diversity and Democracy',
-    name: 'Glen Weyl',
+    name: 'Glen Weyl ◖ RadicalxChange',
     calendar: ''
   },
   {
     time: '13:00 PM',
     title: 'Exploring a potential wormhole between Quadratic Funding and Quantum Mechanics',
-    name: 'Michael Freedman',
+    name: 'Michael Freedman ◖ Microsoft',
     calendar: ''
   },
   {
     time: '14:00 PM',
     title: 'Building Plural Tech — Lightning Presentations & Panel',
-    name: 'Kevin Owocki, Christine Vandevoorde, Aaron Soskin, Guo Liu',
+    name: 'Kevin Owocki ◖ supermodular, Christine Vandevoorde ◖ govrn.io, Aaron Soskin ◖ govrn.io, Guo Liu ◖ Matters Lab',
     calendar: ''
   },
   {
     time: '15:30 PM',
     title: 'Plural Publics',
-    name: 'Shrey Jaineth',
+    name: 'Shrey Jaineth ◖ Microsoft',
     calendar: ''
   },
   {
     time: '16:00 PM',
     title: 'Deep Dive on DeSoc',
-    name: 'Puja Ohlhaver, Special Guest',
+    name: 'Puja Ohlhaver ◖ Flashbots, Special Guest',
     calendar: ''
   },
 ];
@@ -158,61 +158,61 @@ export const talksFTC = [
   {
     time: '10:30 AM',
     title: 'Intro',
-    name: 'Evan Miyazono',
+    name: 'Evan Miyazono ◖ Protocol Labs',
     calendar: ''
   },
   {
     time: '10:45 AM',
     title: 'Hypercerts: the Data layer for impact markets',
-    name: 'Holke Brammer and Kevin Owocki',
+    name: 'Holke Brammer ◖ Protocol Labs and Kevin Owocki ◖ supermodular',
     calendar: ''
   },
   {
     time: '11:10 AM',
     title: 'Investing in Network goods: new ecosystem funding paradigms',
-    name: 'Joanna Liang, Anna K, Ken Ng, Scott Moore',
+    name: 'Joanna Liang ◖ JSquare, Anna Kryukova ◖ Celo, Ken Ng ◖ Uniswap Foundation, Scott Moore ◖ Gitcoin',
     calendar: ''
   },
   {
     time: '11:50 AM',
     title: 'Impact Evaluator learnings and theory',
-    name: 'Matt Frehlich',
+    name: 'Matt Frehlich ◖ Protocol Labs',
     calendar: ''
   },
   {
     time: '12:15 PM',
     title: 'Radicle Drips - the latest mechanism design',
-    name: 'Andrew Dickson',
+    name: 'Andrew Dickson ◖ Radicle',
     calendar: ''
   },
   {
     time: '12:40 PM',
     title: 'Celo tokenomics - learnings and next steps for nature-backed currencies',
-    name: 'Marek Olszekski',
+    name: 'Marek Olszekski ◖ Celo/cLabs',
     calendar: ''
   },
   {
     time: '1:05 PM',
     title: 'DAO allocation patterns',
-    name: 'Danielo Lessi, Daphne Charles, Pooja Shah',
+    name: 'Danielo Lessi ◖ , Daphne Charles ◖ Kiwi Labs, Pooja Shah ◖ Tephra Labs',
     calendar: ''
   },
   {
     time: '1:35 PM',
     title: 'Funding Structures',
-    name: 'Rene Pinnell, Vaughn Mackenzie',
+    name: 'Rene Pinnell ◖ Artizen Fund, Vaughn Mackenzie ◖ WeGift/JAAK',
     calendar: ''
   },
   {
     time: '2:35 PM',
     title: 'Impact Markets',
-    name: 'Holke Brammer, Rhys Lindmark, Manu Alzuru',
+    name: 'Holke Brammer ◖ Protocol Labs, Rhys Lindmark ◖ Roote/Web3 & Society, Manu Alzuru ◖ DoinGud',
     calendar: ''
   },
   {
     time: '3:35 PM',
     title: 'Funding Mechanisms',
-    name: 'Marcus Aurelius, Livia De',
+    name: 'Marcus Aurelius ◖ KlimaDAO, Livia De ◖ Commons Stack',
     calendar: ''
   },
   {
@@ -224,7 +224,7 @@ export const talksFTC = [
   {
     time: '5:05 PM',
     title: 'End of Day',
-    name: 'Evan Miyazono',
+    name: 'Evan Miyazono ◖ Protocol Labs',
     calendar: ''
   },
 ];
