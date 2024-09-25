9d2b183a15508a0038f8492d8830d42eb7c31fbf
Cristina Lare <100524474+cristinalare@users.noreply.github.com>
2024-09-25 23:19:01 +0300

    copy update

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,8 +10,8 @@ function About () {
           <img className="aboutBg" src={aboutBg} alt="" />
         </Parallax>
         <div className='about-content'>
-            <h2 className='main-xxl yellow italic joy'>The Joy of <br/>Collective Thriving</h2>
-            <p className='description main-l'>An Unconference For Planetary <br/>Wellbeing &amp;  Flourishing</p>
+            <h2 className='main-xxl yellow italic joy'>Details <br/>coming soon</h2>
+            <p className='description main-l'>We will be announcing the details<br/>by January</p>
         </div>
       </div>
     );
